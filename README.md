58dce17ecde31f5b94e334caacb482ea72f8431a
miandry <miandrilala9@yahoo.fr>
2018-01-16 02:27:24 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# recycle
-Drupal 8 recycle Demo
+1- install miandry_recycle_bin
+2- Importe Config in /config/views.view.recycle.yml
+   (use Back end Views to build the list)
+Notice
+------
+
+url : /admin/recyles
+
---
+++ modules/views_bulk_operations_recycle/src/Plugin/Action/RecycleAction.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\views_bulk_operations_recycle\Plugin\Action;
+
+use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
+use Drupal\views_bulk_operations\Action\ViewsBulkOperationsPreconfigurationInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+
+use Drupal\taxonomy\Entity\Term;
+/**
+ * An example action covering most of the possible options.
+ *
+ * If type is left empty, action will be selectable for all
+ * entity types.
+ *
+ * @Action(
+ *   id = "views_bulk_operations_recycle",
+ *   label = @Translation("Restore Entity"),
+ *   type = "",
+ *   confirm = TRUE,
+ *   pass_context = TRUE,
+ *   pass_view = TRUE
+ * )
+ */
+class RecycleAction extends ViewsBulkOperationsActionBase implements ViewsBulkOperationsPreconfigurationInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    /*
+     * All config resides in $this->configuration.
+     * Passed view rows will be available in $this->context.
+     * Data about the view used to select results and optionally
+     * the batch context are available in $this->context or externally
+     * through the public getContext() method.
+     * The entire ViewExecutable object  with selected result
+     * rows is available in $this->view or externally through
+     * the public getView() method.
+     */
+
+    // Do some processing..
+    // ...
+
+
+       $entity->edited = 0;
+       $entity->save();
+       drupal_set_message($entity->id());
+
+    return sprintf('Update status recycle action success');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildPreConfigurationForm(array $form, array $values, FormStateInterface $form_state) {
+    return $form;
+  }
+  /**
+   * Configuration form builder.
+   *
+   * If this method has implementation, the action is
+   * considered to be configurable.
+   *
+   * @param array $form
+   *   Form array.
+   * @param Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object.
+   *
+   * @return array
+   *   The configuration form.
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+      return $form;
+  }
+
+  /**
+   * Submit handler for the action configuration form.
+   *
+   * If not implemented, the cleaned form values will be
+   * passed direclty to the action $configuration parameter.
+   *
+   * @param array $form
+   *   Form array.
+   * @param Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object.
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+ 
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    if ($object->getEntityType() === 'node') {
+      $access = $object->access('update', $account, TRUE)
+        ->andIf($object->status->access('edit', $account, TRUE));
+      return $return_as_object ? $access : $access->isAllowed();
+    }
+    return TRUE;
+  }
+
+}
